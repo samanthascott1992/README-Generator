1e05f9f3da008a38e4b810c19e98afd10084a7bc
Samantha Scott <Samanthascott1992@gmail.com>
2020-04-15 19:18:58 -0600

    added things from activities

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
-const questions = [
+// const questions = [
 
-];
+// ];
 
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 
-function init() {
+// function init() {
 
-}
+// }
 
-init();
+// init();
+
+// // Activity 06
+
+// var fs = require("fs");
+
+// fs.writeFile("log.txt", process.argv[2], function(err) {
+
+//   if (err) {
+//     return console.log(err);
+//   }
+
+//   console.log("Success!");
+
+// });
+
+// Activity 14
+
+var inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username"
+    },
+    {
+      type: "password",
+      message: "What is your password?",
+      name: "password"
+    },
+    {
+      type: "password",
+      message: "Re-enter password to confirm:",
+      name: "confirm"
+    }
+  ])
+  .then(function(response) {
+
+    if (response.confirm === response.password) {
+      console.log("Success!");
+    }
+    else {
+      console.log("You forgot your password already, " + response.username + "?!");
+    }
+  });
+
