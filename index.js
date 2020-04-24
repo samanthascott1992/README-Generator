d8656511018c98f6dc9bddfc9bcfed6dbcb09b3d
Samantha Scott <Samanthascott1992@gmail.com>
2020-04-23 19:08:15 -0600

    fixed license generation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const generateMarkdown = require("./utils/generateMarkdown");
+const license = require("./license.js");
 
 const questions = [
     {
@@ -30,16 +31,22 @@ const questions = [
         name: "use",
         message: "What is the intended usage for this product?"
         },   
-       {
-        type: "input",
-        name: "FIXME",
-        message: "What kind of license should your project have?"
 
-        },
+        {
+        type: "input",
+        name: "contributors",
+        message: "If you had any contributors, please list them here:"
+          },
         {
         type: "input",
         name: "command",
-        message: "What command should I run to install all dependencies?"
+        message: "What command should the user run to install all dependencies?"
+        }, 
+        {
+        type: "checkbox",
+        name: "FIXME",
+        message: "What kind of license should your project have?",
+        choices: ["MIT","GNU"]
         },
 ];
 
@@ -56,10 +63,23 @@ async function init() {
   console.log("hi")
   try {
     const answers = await promptUser();
-    console.log(answers);
-    const text = generateMarkdown(answers);
+    console.log(answers);  
+    const licenseInAnswers = {'github' : answers['github'], 
+                              'title' : answers['title'],
+                              'projectUrl' : answers['projectUrl'],
+                              'description' : answers['description'],
+                              'use' : answers['use'],
+                              'contributors' : answers['contributors'], 
+                              'command' : answers['command'], 
+                              'FIXME' : license(answers['FIXME'])
+                            };
+    console.log(licenseInAnswers);
+    
+    const text = generateMarkdown(licenseInAnswers);
+
 
     await writeFileAsync("README.md", text);
+    // await writeFileAsync("LICENSE",licenseInfo);
 
     console.log("Successfully wrote to README.md");
   } catch(err) {
--- license.js
+++ license.js
@@ -1,31 +1,34 @@
 
-# Project Title
----------------
-fgh
+const answers = require("./index.js")
+
+const mitLicense = `MIT License
+
+Copyright (c) [year] [fullname]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.`;
 
 
 
-## Description
----------------
-fg
 
 
-### GitHub Username
-------------------
-fxg
 
-## Project Repo URL
--------------------
- fdgh
-
-## Intended Use for Product
---------------------------
- fg
-
-
-### Which type of license do you wish to use?
-----------------------------
-GNU GENERAL PUBLIC LICENSE
+const gnuLicense = `GNU GENERAL PUBLIC LICENSE
                        Version 3, 29 June 2007
 
  Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
@@ -698,9 +701,17 @@ into proprietary programs.  If your program is a subroutine library, you
 may consider it more useful to permit linking proprietary applications with
 the library.  If this is what you want to do, use the GNU Lesser General
 Public License instead of this License.  But first, please read
-<https://www.gnu.org/licenses/why-not-lgpl.html>.
+<https://www.gnu.org/licenses/why-not-lgpl.html>.`;
 
-### What command do you use to install?
-------------------------
- dfrtg
 
+
+
+function writeLicense(license) {
+
+  // if (answers[7].choices[0]) return mitLicense;
+  // if (answers[7].choices[1]) return gnuLicense;
+  if(license == 'MIT') return mitLicense;
+  else if(license == 'GNU') return gnuLicense;
+}
+
+module.exports = writeLicense;
